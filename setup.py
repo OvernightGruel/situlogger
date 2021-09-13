c56dd849343f893777d302efda937710f626890b
周伟奇 <orzzwq@gmail.com>
2021-09-13 19:03:37 +0800

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='situlogger',
+      version='0.1',
+      description='situ logger',
+      author='clay',
+      author_email='zhouweiqi@situdata.com',
+      packages=['situlogger'],
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "Operating System :: OS Independent",
+      ],)
